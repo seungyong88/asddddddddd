dc23ce413b3ce5c3055f155099e4298798607c91
seungyong88 <gosng2001@naver.com>
2023-10-14 21:15:33 +0900

    asdsad

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-const staticDir = path.join(__dirname, './build');
+const staticDir = path.join(__dirname, '../build');
 const spaIndex = path.join(staticDir, 'index.html');
 
 app.use(express.static(staticDir));
@@ -14,5 +14,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.info(`Express server started: http://localhost:${port}`);
+  console.info(`Express server started: http://localhost:${port}`);
 });
