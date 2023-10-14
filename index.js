fde6c970a99ebeb4d7576662d163d83686360be8
seungyong88 <gosng2001@naver.com>
2023-10-14 21:18:26 +0900

    asdsad

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-const staticDir = path.join(__dirname, './build');
+const staticDir = path.join(__dirname, '/build');
 const spaIndex = path.join(staticDir, 'index.html');
 
 app.use(express.static(staticDir));
